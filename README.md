c7d141730457ec615663cef427e10d39892c1978
Jorge Visca <jvisca@fing.edu.uy>
2019-08-23 12:55:30 -0300

    updated API, better test script, improved memory usage

--- README.md
+++ README.md
@@ -13,21 +13,25 @@ Transparent persistence for Lua data! Seriously!
 How?
 ----
 
-```
+```lua
 -- load library
 local persistor = require 'persistor'
 
 -- get a persistor table.
 -- if the root folder exists, data will be read from there.
 -- if the name is omitted "$PWD/tmp-persistor" will be used.
--- remember: whatever is inside this folder can be purged.
+-- if there was data from previous executions, it will still be there.
+-- data inside this folder is used at will, do not use it for anything else
 local p = persistor.new('path/to/root/folder')
+
+-- also you can purge all stored data loading the persistor table as follows:
+-- local p =  persistor.new('path/to/root/folder', true)
 
 -- store something
 p.pi = 3.14
 p.circle = {center={x=0, y=0}, radius=0.5}
 
--- read something (possibly much later)
+-- read something (possibly much later, in another run)
 print (2 * p.pi * p.circle.radius)
 ```
 
--- test.lua
+++ test.lua
@@ -1,16 +1,30 @@
 local persistor = require 'persistor'
 
-local p = persistor.new()
+local lfs = require 'lfs'
+
+local path = assert(lfs.currentdir())..'/tmp-persistor'
+print ('using '..path..' for data')
+
+local p
+
+---[[
+--Create a new persistent table, ensure it's clear
+p = persistor.new(path, true)
 p.aaa = {} 
-local ret = p.aaa
-print ('read1->', ret)
-print ('comp1->', ret==p.aaa)
-ret.bbb = 'lorem ipsum'
-print ('read2->', type(ret.bbb), ret.bbb)
-ret.ccc = 3.14
-print ('read3->', type(ret.ccc), ret.ccc)
-p.aaa.ddd={eee=true}
+p.bbb = 'lorem\nipsum'
+p.ccc = 3.14
+p.aaa.ddd={eee=false}
+--]]
+
+---[[
+-- Use the stored data
+p = nil;collectgarbage();collectgarbage()
+p = persistor.new(path)
+print ('read1->', p.aaa)
+print ('read2->', type(p.bbb), p.bbb)
+assert(p.bbb == 'lorem\nipsum')
+print ('read3->', type(p.ccc), p.ccc)
+assert(p.ccc == 3.14)
 print ('read4->', type(p.aaa.ddd.eee), p.aaa.ddd.eee)
-ret.bbb= nil
-assert (ret.bbb == nil)
-assert (type(ret.bbb) == 'nil') 
+assert(p.aaa.ddd.eee==false)
+--]]
